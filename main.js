6b0c00251840a78c9a81d458d5fc6204f82597e5
tungning24 <tungning.p@gmail.com>
2026-06-16 14:57:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -236,6 +236,19 @@ function renderStandings(){
     html += `
     <div class="match">
     <table style="width:100%;font-size:13px;border-collapse:collapse;table-layout:fixed">
+
+      <colgroup>
+        <col style="width:38%">
+        <col style="width:7%">
+        <col style="width:7%">
+        <col style="width:7%">
+        <col style="width:7%">
+        <col style="width:7%">
+        <col style="width:7%">
+        <col style="width:10%">
+        <col style="width:10%">
+      </colgroup>
+
       <tr>
         <th style="text-align:left">Team</th>
         <th style="text-align:center">P</th>
@@ -254,23 +267,23 @@ function renderStandings(){
 
       html += `
       <tr>
-        <td style="text-align:left;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;width:140px">
+        <td style="text-align:left;white-space:nowrap;overflow:hidden;text-overflow:ellipsis">
           <img src="flags/${flag}.jpg"
           style="width:18px;height:12px;vertical-align:middle;margin-right:6px"
           onerror="this.src='flags/none.png'">
           ${t.name}
         </td>
 
-        <td style="text-align:center;width:35px">${t.played}</td>
-        <td style="text-align:center;width:35px">${t.win}</td>
-        <td style="text-align:center;width:35px">${t.draw}</td>
-        <td style="text-align:center;width:35px">${t.lose}</td>
+        <td style="text-align:center">${t.played}</td>
+        <td style="text-align:center">${t.win}</td>
+        <td style="text-align:center">${t.draw}</td>
+        <td style="text-align:center">${t.lose}</td>
 
-        <td style="text-align:center;width:35px">${t.gf}</td>
-        <td style="text-align:center;width:35px">${t.ga}</td>
+        <td style="text-align:center">${t.gf}</td>
+        <td style="text-align:center">${t.ga}</td>
 
-        <td style="text-align:center;width:35px">${t.gf - t.ga}</td>
-        <td style="text-align:center;width:40px"><b>${t.pts}</b></td>
+        <td style="text-align:center">${t.gf - t.ga}</td>
+        <td style="text-align:center"><b>${t.pts}</b></td>
       </tr>`;
     });
 
